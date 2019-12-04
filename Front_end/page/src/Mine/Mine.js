d4560c9c0c98474c311b9407fabfcb9cf9445b28
zhangxiaohuan8 <2916244782@qq.com>
2019-12-04 16:52:58 +0800

    a

--- Front_end/page/src/Mine/Mine.js
+++ Front_end/page/src/Mine/Mine.js
@@ -10,47 +10,141 @@ export default class Mine extends Component {
     constructor(){
         super();
         this.state={
+            // 判断用户是否登录
             flag:store.getState().loginstateflag,
             userid: store.getState().LoginchangeValueName,
-            data:""
+            // 判断作品，收藏，发布哪一个显示
+            judge:1,
+            // 用户的信息
+            user_data:"",
+            // 用户作品，收藏，发布的信息
+            offpaint_data:[]
         }
     }
     componentDidMount() {
         if(this.state.flag == true){
             fetch('http://xiawx.top:8080/personal?userid='+this.state.userid)
             .then(res=>res.json())
-            .then((data)=>{
+            .then((res)=>{
                 this.setState({
-                    data:data.content[0]
+                    user_data:res.content[0]
                 })
-                console.log(this.state.data.userid);
             })
+            this.sendrequest('work',this.state.userid,0);
         }
+    }
+    sendrequest=(choose,userid,id)=>{
+        let url = 'http://xiawx.top:8080/'+choose+'?userid='+userid;
+        fetch(url)
+        .then(res=>res.json())
+        .then(res=>{
+            var arr = [];
+            if(res.content.length>4){
+                arr = res.content.slice(0,4);
+            }
+            else{
+                arr = res.content;
+            }
+            this.setState({
+                 offpaint_data:arr
+            })
+            // console.log(this.state.offpaint_data);
+            // console.log(typeof this.state.offpaint_data);
+            for(var i=0;i<this.state.offpaint_data.length;i++){
+                var canvas = document.getElementById('canvas'+id+i);
+                var context=canvas.getContext("2d");
+                var a = new window.Picture;
+                
+                a.prase(this.state.offpaint_data[i].paintdata);
+                a.drawDataMatrix=a.prase(this.state.offpaint_data[i].paintdata);
+                a.initWH(canvas.width,canvas.height);
+                a.draw(context)
+                // a.inittable(context)
+            }
+        })
+    }
+    change0=()=>{
+        this.setState({
+            judge:1
+        })
+        if(this.state.flag == true){
+            this.sendrequest('work',this.state.user_data.userid,0);
+        }
+    }
+    change1=()=>{
+        this.setState({
+            judge:2
+        })
+        if(this.state.flag == true){
+            this.sendrequest('collection',this.state.user_data.userid,1);
+        }
+    }
+    // 没有发布的接口还没改
+    change2=()=>{
+        this.setState({
+            judge:3
+        })
+        fetch('http://xiawx.top:8080/offpaint')
+        .then(res=>res.json())
+        .then(res=>{
+            var arr = [];
+            if(res.content.length>4){
+                arr = res.content.slice(0,4);
+            }
+            else{
+                arr = res.content;
+            }
+            this.setState({
+                 offpaint_data:arr
+            })
+            // console.log(this.state.offpaint_data);
+            // console.log(typeof this.state.offpaint_data);
+            for(var i=0;i<this.state.offpaint_data.length;i++){
+                var canvas = document.getElementById('canvas2'+i);
+                var context=canvas.getContext("2d");
+                var a = new window.Picture;
+                
+                a.prase(this.state.offpaint_data[i].paintdata);
+                a.drawDataMatrix=a.prase(this.state.offpaint_data[i].paintdata);
+                a.initWH(canvas.width,canvas.height);
+                a.draw(context)
+                // a.inittable(context)
+            }
+        })
     }
     render(){
         return (
         <div className="mine">
             <div className="mine_navbar_div">
                 <p className="mine_navbar">我 的</p>
+                <i className="iconfont icon-set"></i>
+                <div className="mine_clearfloat"></div>
             </div>
+            {/* 个人信息 */}
             <div className="mine_message_div">
                 <WingBlank>
+                    {/* 登录状态 */}
                     <div className="mine_message" style={{display:this.state.flag?"block":"none"}}>
                         <div id="user-photo">
                             <img className="mine_message_left" src={this.state.filePhoto || "img/mine_message_img.png"}/>
                         </div>
                         <div className="mine_message_right">
-                            <div className="mine_mesage_name">{this.state.data.userid}</div>
-                            <div className="mine_mesage_sign">TA还没有个性签名</div>
+                            <div className="mine_message_name">{this.state.user_data.userid}</div>
+                            <div className="mine_message_sign">TA还没有个性签名</div>
                         </div>
                         <div className="mine_clearfloat"></div>
                     </div>
+                    {/* 未登录状态 */}
                     <div className="mine_message" style={{display:this.state.flag?"none":"block"}}>
                     <div id="user-photo">
                             <img className="mine_message_left" src={this.state.filePhoto || "img/mine_message_img.png"}/>
                         </div>
                         <div className="mine_message_right">
-                            <div className="mine_mesage_none" onClick={this.handle}>未登录</div>
+                            <div className="mine_message_name" onClick={this.handle}>未登录</div>
+                            <div className="mine_message_sign">
+                                <Link to="/denglu">登录</Link>/
+                                <Link to="/regsiter">注册</Link>
+                            </div>
                         </div>
                         <div className="mine_clearfloat"></div>
                     </div>
@@ -63,24 +157,24 @@ export default class Mine extends Component {
                     <div className="mine_three">
                         <Flex>
                             <Flex.Item>
-                                <Link className="mine_works" to="/opus">
+                                <div className="mine_works" onClick={this.change0}>
                                     <i className="iconfont icon-zuopin"></i>
                                     <p>我的作品</p>
-                                </Link>
+                                </div>
                             </Flex.Item>
 
                             <Flex.Item>
-                                <Link className="mine_works" to="/collection">
+                                <div className="mine_works" onClick={this.change1}>
                                     <i className="iconfont icon-shoucang"></i>
                                     <p>我的收藏</p>
-                                </Link>
+                                </div>
                             </Flex.Item>
                             
                             <Flex.Item>
-                                <Link className="mine_works" to="/release">
+                                <div className="mine_works" onClick={this.change2}>
                                     <i className="iconfont icon-fabu"></i>
                                     <p>我的发布</p>
-                                </Link>
+                                </div>
                             </Flex.Item>
                         </Flex>
                         
@@ -88,39 +182,67 @@ export default class Mine extends Component {
                     </div>
                 </WingBlank>
             </div>
-            
+
             <WhiteSpace size="md" />
-            <div className="mine_bottom_div">
-                <WingBlank>
-                    <div className="mine_bottom">
-                        <div className="mine_bottom_setup_div">
-                            <i className="iconfont icon-set"></i>
-                            <div className="mine_bottom_setup">
-                                <Link className="mine_bottom_link" to="/database">设置</Link>
-                            </div>
-                            <div className="mine_clearfloat"></div>
-                        </div>
-                        
-                        <div className="mine_bottom_setup_div">
-                            <i className="iconfont icon-set"></i>
-                            <div className="mine_bottom_setup">
-                                <Link className="mine_bottom_link" to="/regsiter">注册</Link>
-                            </div>
-                            <div className="mine_clearfloat"></div>
-                        </div>
-                        
-                        <div className="mine_bottom_setup_div">
-                            <i className="iconfont icon-set"></i>
-                            <div className="mine_bottom_setup">
-                                <Link className="mine_bottom_link" to="/denglu">登录</Link>
-                            </div>
-                            <div className="mine_clearfloat"></div>
-                        </div>
+            <div className="mine_show_div" style={{display:this.state.flag?"block":"none"}}>
+            <WingBlank>
+                {/* 我的作品 */}
+                <div className="mine_show_opus" style={{display:this.state.judge==1?"block":"none"}}>
+                    <div className="mine_show_no1">
+                        <p className="mine_show_word_no1">我的作品</p>
+                        <Link to="/opus"><p className="mine_show_word_no2">查看更多>></p></Link>
+                        <div className="mine_clearfloat"></div>
                     </div>
-                </WingBlank>
+                    {
+                        this.state.offpaint_data.map((item,idx)=>
+                            <div className="mine_content">
+                                <canvas className="mine_content_canvas" 
+                                id={"canvas0"+idx} width='400px'height='400px'>
+                                        
+                                </canvas>
+                            </div>
+                        )
+                    }
+                </div>
+                {/* 我的收藏 */}
+                <div className="mine_show_opus" style={{display:this.state.judge==2?"block":"none"}}>
+                    <div className="mine_show_no1">
+                        <p className="mine_show_word_no1">我的收藏</p>
+                        <Link to="/collection"><p className="mine_show_word_no2">查看更多>></p></Link>
+                        <div className="mine_clearfloat"></div>
+                    </div>
+                    {
+                        this.state.offpaint_data.map((item,idx)=>
+                            <div className="mine_content">
+                                <canvas className="mine_content_canvas" 
+                                id={"canvas1"+idx} width='400px'height='400px'>
+                                        
+                                </canvas>
+                            </div>
+                        )
+                    }
+                </div>
+                {/* 我的发布 */}
+                <div className="mine_show_opus" style={{display:this.state.judge==3?"block":"none"}}>
+                    <div className="mine_show_no1">
+                        <p className="mine_show_word_no1">我的发布</p>
+                        <Link to="/release"><p className="mine_show_word_no2">查看更多>></p></Link>
+                        <div className="mine_clearfloat"></div>
+                    </div>
+                    {
+                        this.state.offpaint_data.map((item,idx)=>
+                            <div className="mine_content">
+                                <canvas className="mine_content_canvas" 
+                                id={"canvas2"+idx} width='400px'height='400px'>
+                                        
+                                </canvas>
+                            </div>
+                        )
+                    }
+                </div>
+            </WingBlank>
             </div>
-            <div className="mine_hight">
-            </div>
+            <div className="mine_hight"></div>
             <Undertab flag="3"/>
         </div>
         )
--- Front_end/page/src/draw/drawing.js
+++ Front_end/page/src/draw/drawing.js
@@ -10,6 +10,20 @@ export default function Drawing(props) {
     var [colorlist, setColorlist] = useState([])
     var [obj, setObj] = useState(pic)
     var pic = new window.Picture();
+    var [color, setColor] = useState(true);
+    var [tool, setTool] = useState(true);
+    function Changefree(){
+        setColor(false);  
+    }
+    function Changetuijian(){
+        setColor(true); 
+    }
+    function ToolSide(){
+        setTool(true);  
+    }
+    function Tool(){
+        setTool(false); 
+    }
     useEffect(() => {
         // setPicdata("#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#eee00e#eee00e#eee00e#eee00e#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#eee00e#eee00e#eee00e#ffffff#ffffff#ffffff#ffffff#eee00e#eee00e#eee00e#ffffff#ffffff#ffffff#eee00e#ffffff#ffffff#ffffff#eee00e#eee00e#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#ffffff#ffffff#eee00e#eee00e#ffffff#eee00e#eee00e#eee00e#ffffff#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#eee00e#eee00e#eee00e#ffffff#eee00e#eee00e#ffffff#ffffff#eee00e#ffffff#eee00e#ffffff#eee00e#ffffff#ffffff#eee00e#ffffff#ffffff#ffffff#eee00e#eee00e#ffffff#eee00e#eee00e#eee00e#eee00e#ffffff#ffffff#eee00e#eee00e#eee00e#ffffff#eee00e#ffffff#ffffff#eee00e#ffffff#ffffff#eee00e#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#eee00e#eee00e#eee00e#eee00e#ffffff#ffffff#eee00e#ffffff#ffffff#eee00e#eee00e#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#ffffff#ffffff#ffffff#eee00e#ffffff#ffffff#eee00e#ffffff#ffffff#ffffff#eee00e#eee00e#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#eee00e#ffffff#ffffff#ffffff#eee00e#ffffff#eee00e#eee00e#ffffff#ffffff#ffffff#ffffff#eee00e#eee00e#ffffff#ffffff#eee00e#eee00e#eee00e#eee00e#ffffff#ffffff#ffffff#ffffff#eee00e#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#eee00e#eee00e#eee00e#eee00e#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#eee00e#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#eee00e#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#eee00e#ffffff#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#eee00e#eee00e#eee00e#eee00e#eee00e#eee00e#eee00e#eee00e#eee00e#eee00e#ffffff#ffffff#ffffff#eee00e#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff#ffffff")
         var canvas = document.getElementById("canvas");
@@ -57,11 +71,6 @@ export default function Drawing(props) {
     }
 
     function finishDraw(){
-        // //上传数据
-        // fetch(url,{
-        //     method:"POST",
-        //     body:JSON.stringify({data:obj.toString})
-        // })
         obj.clearCanvas(obj.context);
         obj.draw(obj.context);
         // obj.automaticPainting(obj.context);
@@ -70,14 +79,11 @@ export default function Drawing(props) {
         var img=obj.convertCanvasToImage(obj.context);
         document.getElementById("show").appendChild(img)
     }
-
     var aa;
-    // var aa = useState(0);
-    // window.onresize = function(){
         var w=window.outerWidth;
         if(w<700){aa=300}
-        else if(700<w&&w<900){aa=650}
-        else if(900<w){aa=850}
+        else if(700<w&&w<900){aa=500}
+        else if(900<w){aa=700}
     // }
     return (
     <div className="drawing">
@@ -85,16 +91,52 @@ export default function Drawing(props) {
             <div className="drawing_canvas_div">
                 <canvas id="canvas" width={aa} height={aa}>您的浏览器版本过低</canvas>
             </div>
-            
-            <div className="drawing_body">
-                <div className="drawing_title">
-                    <p className="drawing_word">推荐色</p>
+        </div>
+        <div className="drawing_show" id="show" style={{float:'left',marginTop:"10vw"}}>
+            <p className="drawing_word">生成图片保存</p>
+        </div>
+        <div className="drawing_shezhi_no1"  onClick={Tool}>
+            <div className="drawing_btn">
+                <button onClick={showNumber}>
+                    工具
+                </button>
+            </div>
+        </div>
+        <div className="drawing_shezhi" style={{display:tool?"none":"block"}}>
+            <div className="drawing_shezhi_no2">
+                <div className="drawing_btn">
+                    <button onClick={ToolSide}>
+                        收回
+                    </button>
                 </div>
-                <div className="drawing_main">
+                <div className="drawing_btn">
+                    <button onClick={showNumber}>
+                            显示数字
+                        </button>
+                </div>
+                <div className="drawing_btn">
+                    <button onClick={recall}>
+                        撤回
+                    </button>
+                </div>
+                <div className="drawing_btn">
+                    <button onClick={finishDraw}>
+                        完成
+                    </button>
+                </div>
+            </div>
+             
+        </div>
+        <div className="drawing_bottom">
+            <div className="drawing_left">
+                <div className="drawing_tuijian" onClick={Changetuijian} style={{color:color?"rgb(110,199,194)":"black"}}>推荐配色</div>
+                <div className="drawing_free" onClick={Changefree}>自由配色</div>
+            </div>
+            <div className="drawing_right">
+                <div style={{display:color?"block":"none"}}>
                     <div className="drawing_choose">
-                        <div className="drawing_choose_no2">
                         {
-                            colorlist/*.filter((item)=>item='#ffffff')*/.map((item, idx) =>
+                            colorlist.map((item, idx) =>
                             <div key={idx} className="drawing_color">
                                 <div className="drawing_color_div" 
                                 onClick={() => { changeColor(item) }} 
@@ -104,48 +146,15 @@ export default function Drawing(props) {
                                 </div>
                             </div>
                             )
-                        }
-                       </div>
+                         }
                     </div>
                 </div>
-            </div>
-
-            <div className="drawing_body">
-                <div className="drawing_title">
-                    <p className="drawing_word">自由选色</p>
-                </div>
-                <div className="drawing_main">
-                    <div className="drawing_color">
+                <div>
+                    <div className="drawing_color" style={{display:color?"none":"block"}}>
                         <Palette change={changeColorFree} />
                     </div>
                 </div>
             </div>
-
-            <div className="drawing_body">
-                <div className="drawing_title">
-                    <p className="drawing_word">绘图设置</p>
-                </div>
-                <div className="drawing_main">
-                    <div className="drawing_btn">
-                        <button onClick={showNumber}>
-                            显示数字
-                        </button>
-                    </div>
-                    <div className="drawing_btn">
-                        <button onClick={recall}>
-                            撤回
-                        </button>
-                    </div>
-                    <div className="drawing_btn">
-                        <button onClick={finishDraw}>
-                            完成
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="drawing_show" id="show" style={{float:'left',marginTop:"10vw"}}>
-            <p className="drawing_word">生成图片保存</p>
         </div>
     </div>
     )
