1b29def8364791d394c6194c52bc6501bfb46650
sihan9 <1060192071@qq.com>
2019-12-06 10:15:32 +0800

    修改 splashimg api

--- ndoejs_api/api_serve/Routes/splashimg.js
+++ ndoejs_api/api_serve/Routes/splashimg.js
@@ -8,10 +8,14 @@ const fs = require('fs');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    var num = Math.floor(Math.random()*7);
+    const files = fs.readdirSync(__dirname+'/img');
+    var num = Math.floor(Math.random()*files.length);
     // console.log(num);
+    // console.log(files[0]);
+    // console.log(files.length);
+
     res.setHeader('Access-Control-Allow-Origin', '*');
-    fs.readFile(__dirname+'/img/'+num+'.jpeg','binary',(err, file) => {
+    fs.readFile(__dirname+'/img/'+files[num],'binary',(err, file) => {
         if (err)  {
             console.log(err);
             return;
