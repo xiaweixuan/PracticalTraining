ddd8a687b73fe5b7948e37d868128cbf00ab0f21
litiegen <517206296@qq.com>
2019-12-16 20:38:54 +0800

    历史配色

--- Front_end/page/src/draw/drawing.js
+++ Front_end/page/src/draw/drawing.js
@@ -57,8 +57,8 @@ export default function Drawing(props) {
     }
 
     function changeColorFree(color) {
-        console.log("自由改颜色");
         obj.color = color;
+        console.log(obj.color);
     }
 
     function showNumber(){
@@ -171,11 +171,16 @@ export default function Drawing(props) {
                 <p onClick={finishDraw} class="bottom3 bt-box-p">完成</p>
             </div>
 
-
+            
             <div className="drawing_bottom" style={{display:win?'block':'none'}}> 
                 <div className="drawing_left">
-                    <div className="drawing_tuijian" onClick={Changetuijian} style={{color:color?"rgb(110,199,194)":"black"}}>推荐配色</div>
-                    <div className="drawing_free" onClick={Changefree} style={{color:color?"black":"rgb(110,199,194)"}}>自由配色</div>
+                    <div className="drawing_tuijian" 
+                    onClick={Changetuijian} 
+                    style={{color:color?"rgb(110,199,194)":"black"}}>推荐配色</div>
+                    <div className="drawing_free" 
+                    onClick={Changefree} 
+                    style={{color:color?"black":"rgb(110,199,194)"}}>自由配色</div>
+                    
                 </div>
                 <div className="drawing_right">
                     <div style={{display:color?"block":"none"}}>
@@ -197,7 +202,7 @@ export default function Drawing(props) {
                     </div>
                     <div>
                         <div className="drawing_color" style={{display:color?"none":"block"}}>
-                            <Palette change={changeColorFree} />
+                            <Palette color={obj.color} change={changeColorFree} />
                         </div>
                     </div>
                 </div>
