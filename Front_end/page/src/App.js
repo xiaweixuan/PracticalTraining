e40efecbdcf3a6165579991c022dfeb467d96d4c
litiegen <517206296@qq.com>
2019-12-17 14:38:08 +0800

    页面

--- Front_end/page/src/App.js
+++ Front_end/page/src/App.js
@@ -5,7 +5,7 @@ import Routes from './routes';
 import Drawing from './draw/drawing'
 
 import ToRelease from './draw/toRelease'
-import Npc from './Npc/Npc'
+import Npc from './npc/npc'
 class App extends Component {
 	constructor(props) {
 		super(props);
